2809f64436dc897c85c81dadf5b4b4929f308a31
Michael Kane <michaelpatrickgilliskane@gmail.com>
2017-11-13 23:48:21 +0000

    Working client

--- FYP/roomOccupancy.py
+++ FYP/roomOccupancy.py
@@ -2,30 +2,40 @@
 
 # webserver curl -F "file=@/Users/michaelkane/Desktop/blackSignature.png" http://192.168.0.15:5000/"
 # import the necessary packages:
+# references: https://gist.github.com/yoavram/4351498
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from matplotlib import image as image
 import easygui
-
+import requests
+import base64
 class roomOccupancy():
 	
+	
+	
 	def getImage(self):
+		url = "http://78.18.97.94:5000/"
+		image = cv2.imread('test2.jpg')
+		cv2.imwrite('test.jpg', image)
+		
+		fin = open('test.jpg', "rb")
+		encoded_image = base64.b64encode(fin.read())
+		files={'file': fin}
+		
 		try:
-			image = cv2.imread(file)
-			
-		except:
-			print("User failed to select an image.")
-		while True:
-			# Showing an image on the screen (OpenCV):
-			cv2.imshow("Image", image)
-			key = cv2.waitKey(0)
-
-			# if the 'q' key is pressed, quit:
-			if key == ord("q"):
-				break
+			r = requests.post(url, files=files)
+			print r.text
+		finally:
+			fin.close()
 
 if __name__ == "__main__":
 	result = roomOccupancy()
 	result.getImage()
+
+#http://docs.python-requests.org/en/latest/user/quickstart/#post-a-multipart-encoded-file
+
+# url = "http://michaelkanefyp.dynu.net"
+# with open('References.txt', 'rb') as f: r = requests.post("http://78.18.97.94:5000", file={'References.txt': f})
+# print r.text
 	
