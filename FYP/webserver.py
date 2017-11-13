a8cdd4795a82ac15c23c7c527322326a54bf9747
Michaelkane428 <michaelkane428@gmail.com>
2017-11-13 23:43:09 +0000

    Working webserver, receiving posts from devices outside the network.

--- FYP/webserver.py
+++ FYP/webserver.py
@@ -16,31 +16,40 @@
 '''
 import os
 from flask import Flask, render_template, request, url_for
+from flask_cors import CORS
 
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = 'D:\\Python\\Room-Occupancy-Measurment\\FYP\\Uploads\\'
 ALLOWED_EXTENSIONS = ['jpg']
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+CORS(app)
 
 def allowedFile(filename):
     return filename[-3:].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route("/", methods=['GET','POST'])
+@app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        print("We made it to step one")
         file = request.files['file']
+        print("We made it to step one.5")
         if file and allowedFile(file.filename):
+            print("We made it to step two")
             filename = file.filename
+            print(filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return url_for('uploaded_file', filename=filename)
+            print("We made it boys")
+            return '''<p> it worked</p>'''
     return '''
-    <p>latest image</p>
-    <img src=>
-    '''
-
-
+  <!doctype html>
+  <title>Upload new File</title>
+  <h1>UPload new File</h1>
+  <form action="" method=post enctype=multipart/form-data>
+  <p><input type=file name=file>
+  <input type=submit value=Upload>
+  </form>
+  '''
 if __name__ == "__main__":
     app.run(host="192.168.0.9", port=5000)
