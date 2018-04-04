2dfb20689ce6cee983e275bbd7a5f24f70c982d2
Michael Kane <michaelpatrickgilliskane@gmail.com>
2018-04-04 02:02:18 +0100

    Gallery finished for localhost, moving to live machine with database. The objective now is to implement database functionality to dynamically fill the slideshows with images, the text area with analytics, create datetime object in python and, allow information to flow back and forth between python and javascript. Currently python is only sending data to js through jinja2

--- FYP/static/gallery.js
+++ FYP/static/gallery.js
@@ -4,9 +4,17 @@ let paths = [];
 //https://stackoverflow.com/questions/11178426/how-can-i-pass-data-from-flask-to-javascript-in-a-template
 //https://api.jquery.com/data/
 //https://stackoverflow.com/questions/21626048/unable-to-pass-jinja2-variables-into-javascript-snippet
+//http://jquery.malsup.com/cycle2/api/
+//http://api.jquery.com/appendto/
+//https://stackoverflow.com/questions/19504018/show-seconds-on-input-type-date-local-in-chrome
 function getImages(){
     paths = $('#path_to_images').data();
-    $('.cycle-slideshow').html("<img src='/static/uploads/" + "daisychain.jpg'>");
+    paths = ['breadboard', 'Goals', 'daisychain'];
+    for (let i=0; i < paths.length; i++){
+        $("<img src='/static/uploads/" + paths[i] + ".jpg'>").appendTo('.cycle-slideshow');
+        $("<img src='/static/uploads/" + paths[i] + ".jpg'>").appendTo('.slideshow');
+    }
+    $('.slideshow').cycle();
 }
 getImages()
 
