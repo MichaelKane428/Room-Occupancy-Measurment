9879fb1d8646cdc3f9deb05c62c961f588a55bbb
Michael Kane <michaelkane428@gmail.com>
2018-04-10 22:58:31 +0000

    Finished image processing code, raspberry pi takes a photo using the camera, the program reads it and posts it to the webserver, any further posts in regard to the code will be commenst and cleaning.

--- FYP/client.py
+++ FYP/client.py
@@ -11,49 +11,50 @@ import easygui
 import requests
 import base64
 import datetime
-
+import time
+import os
 class client():	
 	def getImage(self):
 		try:
-		
-			url = "http://michaelkanefyp.dynu.net:5000/upload/"
-			temp = str(datetime.datetime.now())
-			date = temp.replace(":", "-")
-			date = date.replace(" ", "_")
-			date = date[0:19]
-			
-			#Opening an image from a file:
-			file = easygui.fileopenbox()
-			image = cv2.imread(file)
-			person = self.findperson(image)
-			
-			#reverse = cv2.bitwise_not(person)
-			ROI = cv2.bitwise_and(image,image,mask=person)
-			
-			person,contours,_ = cv2.findContours(person,mode=cv2.RETR_EXTERNAL,method=cv2.CHAIN_APPROX_NONE)
-			test = cv2.drawContours(image, contours, -1, (0,255,0), 3)
-			
-			print(len(contours))
-			
-			face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-			gray = cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY)
-			faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-			print(len(faces))
-			for (x,y,w,h) in faces:
-				ROI = cv2.rectangle(ROI,(x,y),(x+w,y+h),(255,0,0),2)
-
-			cv2.imwrite(date +'.jpg', ROI)
-			fin = open(date +'.jpg', "rb")
-			#encoded_image = base64.b64encode(fin.read())
-			files={'file': fin}
-
-			try:
-				r = requests.post(url, files=files, data={'number_of_people': len(faces)})
-				print (r.text)
-			finally:
-				fin.close()
-			#cv2.imwrite('ROI.jpg', test)
-			cv2.imwrite('skin.jpg', ROI)
+			while (True):
+				time.sleep(600)
+				os.system('sudo raspistill -o image.jpg')
+				url = "http://michaelkanefyp.dynu.net:5000/upload/"
+				temp = str(datetime.datetime.now())
+				date = temp.replace(":", "-")
+				date = date.replace(" ", "_")
+				date = date[0:19]
+				
+				#Opening an image from a file:
+				#file = easygui.fileopenbox()
+				image = cv2.imread("image.jpg", 1)
+				person = self.findperson(image)
+				
+				#reverse = cv2.bitwise_not(person)
+				ROI = cv2.bitwise_and(image,image,mask=person)
+				
+				person,contours,_ = cv2.findContours(person,mode=cv2.RETR_EXTERNAL,method=cv2.CHAIN_APPROX_NONE)
+				test = cv2.drawContours(image, contours, -1, (0,255,0), 3)
+				
+				print(len(contours))
+				
+				face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+				gray = cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY)
+				faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+				print(len(faces))
+				for (x,y,w,h) in faces:
+					ROI = cv2.rectangle(ROI,(x,y),(x+w,y+h),(255,0,0),2)
+	
+				cv2.imwrite(date +'.jpg', ROI)
+				fin = open(date +'.jpg', "rb")
+				#encoded_image = base64.b64encode(fin.read())
+				files={'file': fin}
+	
+				try:
+					r = requests.post(url, files=files, data={'number_of_people': len(faces)})
+					print (r.text)
+				finally:
+					fin.close()
 		except:
 			print("User failed to select an image.")
 
@@ -62,9 +63,9 @@ class client():
 	def findperson(self, image):
 		higher = np.array([255,255,255])
 		lower = np.array([0,40,80])
-		
-		HSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-		
+		print("hello")
+		HSV = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+		print("hello2")
 		tone = cv2.inRange(HSV, lower, higher)
 		shape = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(40,40))
 		erosion = cv2.erode(tone, shape)
